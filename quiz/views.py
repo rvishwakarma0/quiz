fe0e277c29cc40779876cd124dbbe31a1ba0f461
rvishwakarma0 <rvishwakarma00786@gmail.com>
2021-02-04 10:31:47 +0530

    sec

--- quiz/views.py
+++ quiz/views.py
@@ -45,9 +45,16 @@ def answer(request, quiz_id):
 
         qid = request.POST['qid']
         choice = request.POST['choice']
+
         quiz = Quiz.objects.get(id = quiz_id)
         ques = Question.objects.get(id=qid)
         quiz_result = QuizResults.objects.get(student=student, quiz=quiz)
+        try:
+            student_answer = StudentAnswer.objects.get(question=ques, answer=choice, student=student)
+            return HttpResponse('already attempted')
+        except:
+            student_answer = StudentAnswer.objects.create(question=ques, answer=choice, student=student)
+            student_answer.save()
         if ques.answer == choice:
             msg = "correct answer!"
             quiz_result.score += 1
@@ -61,11 +68,13 @@ def result(request, quiz_id):
     student = request.user.student
     quiz = Quiz.objects.get(id = quiz_id)
     quiz_result = QuizResults.objects.get(student=student, quiz=quiz)
+    student_answer = StudentAnswer.objects.all()
     context = {
         'start_time' : str(quiz_result.start_time),
         'end_time' : str(quiz_result.end_time),
         'score' : quiz_result.score,
-        'quiz' : quiz
+        'quiz' : quiz,
+        'student_answer' : student_answer
     }
     return render(request, 'quiz/result.html', context)
 
@@ -77,6 +86,7 @@ def register(request):
         user, created = User.objects.get_or_create(username = username)
         print(password)
         user.set_password(password)
+        user.save()
         student, created = Student.objects.get_or_create(user=user)
         student.save()
         return redirect('/login/')
